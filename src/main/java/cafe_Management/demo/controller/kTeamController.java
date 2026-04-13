6302538c374f6334819c06bd1dd603ab2318c8f9
Srija <srijarayikanti@gmail.com>
2026-04-13 15:04:14 +0530

    KTeam

--- src/main/java/cafe_Management/demo/controller/kTeamController.java
+++ src/main/java/cafe_Management/demo/controller/kTeamController.java
@@ -1,4 +1,56 @@
 package cafe_Management.demo.controller;
 
-public class kTeamController {
+import cafe_Management.demo.enitites.Customer;
+import cafe_Management.demo.enitites.KitchenTeam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public interface kTeamController {
+    @Operation(
+            summary="Fetch all orders",
+            operationId="saveKTeamDetails",
+            tags="KitchenTeam",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "saveCustomer saved successfully",content=@Content(mediaType ="application/json",
+                            schema = @Schema(implementation = KitchenTeam.class))),
+                    @ApiResponse(responseCode = "401",description = "Error occurred",content=@Content(mediaType ="application/json",
+                            schema = @Schema(implementation = KitchenTeam.class)))
+
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/customer/saveKTeamDetails",
+            produces = "application/json",
+            consumes = "application/json"
+    )
+    @CrossOrigin
+    ResponseEntity<?> saveKTeamDetails(@RequestBody KitchenTeam request);
+
+    @Operation(
+            summary="Fetch all orders",
+            operationId="fetchKTeamDetails",
+            tags="KitchenTeam",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "saveCustomer saved successfully",content=@Content(mediaType ="application/json",
+                            schema = @Schema(implementation = KitchenTeam.class))),
+                    @ApiResponse(responseCode = "401",description = "Error occurred",content=@Content(mediaType ="application/json",
+                            schema = @Schema(implementation = KitchenTeam.class)))
+
+            }
+    )
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/customer/fetchKTeamDetails",
+            produces = "application/json"
+    )
+    @CrossOrigin
+    ResponseEntity<?> fetchKTeamDetails();
 }
--- src/main/java/cafe_Management/demo/controller/kTeamControllerImpl.java
+++ src/main/java/cafe_Management/demo/controller/kTeamControllerImpl.java
@@ -1,7 +1,33 @@
 package cafe_Management.demo.controller;
 
+import cafe_Management.demo.Service.kTeamService;
+import cafe_Management.demo.enitites.KitchenTeam;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class kTeamControllerImpl {
+public class kTeamControllerImpl implements kTeamController{
+    private final kTeamService kTeamService;
+
+    public kTeamControllerImpl(kTeamService kTeamService) {
+        this.kTeamService = kTeamService;
+    }
+
+    @Override
+    public ResponseEntity<?> saveKTeamDetails(KitchenTeam request) {
+        try {
+            return kTeamService.saveKTeamDetails(request);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public ResponseEntity<?> fetchKTeamDetails() {
+        try {
+            return kTeamService.fetchKTeamDetails();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
